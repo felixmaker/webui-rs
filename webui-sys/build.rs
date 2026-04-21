ced230d9908aaf6e5ca14fc16169cce265eaf7da
felixmaker <7445790+felixmaker@users.noreply.github.com>
2026-04-21 20:37:42 +0800

    update: webui to 2.5

--- webui-sys/build.rs
+++ webui-sys/build.rs
@@ -1,22 +1,28 @@
+use std::env;
+
 fn main() {
-    cc::Build::new()
-        .define("NO_SSL", "")
-        .define("NDEBUG", "")
-        .define("NO_CACHING", "")
-        .define("NO_CGI", "")
-        .define("USE_WEBSOCKET", "")
-        .include("webui/src/civetweb")
-        .include("webui/src/webview")
-        .include("webui/include")
-        .file("webui/src/civetweb/civetweb.c")
-        .file("webui/src/webui.c")
-        .compile("webui");
+    let dst = cmake::Config::new("webui") // Path to your submodule
+        .define("WEBUI_BUILD_EXAMPLES", "OFF")
+        .define("BUILD_SHARED_LIBS", "OFF")
+        .profile("release")
+        .build();
 
-    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
 
-    if target_os == "windows" {
-        println!("cargo::rustc-link-lib=user32");
-        println!("cargo::rustc-link-lib=shell32");
-        println!("cargo::rustc-link-lib=ole32");
+    println!("cargo:rustc-link-lib=static=webui-2-static");
+
+    let target = env::var("TARGET").unwrap();
+    if target.contains("apple") {
+        println!("cargo:rustc-link-lib=framework=Cocoa");
+        println!("cargo:rustc-link-lib=framework=WebKit");
+    } else if target.contains("windows") {
+        println!("cargo:rustc-link-lib=ws2_32");
+        println!("cargo:rustc-link-lib=user32");
+        println!("cargo:rustc-link-lib=shell32");
+        println!("cargo:rustc-link-lib=ole32");
+        println!("cargo:rustc-link-lib=advapi32");
     }
+
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=webui");
 }
